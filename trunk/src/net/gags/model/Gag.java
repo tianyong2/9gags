ecf6412f1dbce5511f2dfb2a7b7e7c02218655db
vitor.muniz.carvalho@gmail.com <vitor.muniz.carvalho@gmail.com@8c44c937-0077-aa04-3ab2-292d998e9c71>
2012-04-11 02:37:37 +0000

    Implementado a exibição e o armazenamento dos gags no sdcard

--- trunk/src/net/gags/model/Gag.java
+++ trunk/src/net/gags/model/Gag.java
@@ -1,32 +1,70 @@
 package net.gags.model;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+import android.util.Log;
+
 public class Gag
 {
     
-    private int ID;
-    private String URL;
+    private static String localDir; 
     
+    private int id;
+    private String url;
+    private String title;
+    private String localUrl;
     
-    public int getID()
+    static
     {
-        return ID;
+        localDir = Environment.getExternalStorageDirectory().getAbsolutePath()+"/9Gags/gags/";
+        new File(localDir).mkdirs();
     }
     
-    public void setID(int iD)
+    public Gag(int id, String url, String title) throws MalformedURLException, IOException
     {
-        ID = iD;
+        this.id = id;
+        this.url = url;
+        this.title = title;
+        this.localUrl = localDir+id+".jpg";
+        
+        Log.i("LocalURL", localUrl);
+
+//        requestImage();
     }
     
-    public String getURL()
+    public void requestImage() throws MalformedURLException, IOException
     {
-        return URL;
+        Bitmap bitmap = BitmapFactory.decodeStream(new URL(url).openStream());
+        bitmap.compress(CompressFormat.JPEG, 100, new FileOutputStream(localUrl));
     }
     
-    public void setURL(String uRL)
+    public int getId()
     {
-        URL = uRL;
+        return id;
     }
     
+    public String getUrl()
+    {
+        return url;
+    }
     
+    public String getLocalUrl()
+    {
+        return localUrl;
+    }
+    
+    public String getTitle()
+    {
+        return title;
+    }
+
 
 }
