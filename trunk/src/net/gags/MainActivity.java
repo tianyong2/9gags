499703cf98e232c4b57f84f7b9b618679af3df40
vitor.muniz.carvalho@gmail.com <vitor.muniz.carvalho@gmail.com@8c44c937-0077-aa04-3ab2-292d998e9c71>
2012-04-13 01:05:04 +0000

    Adicionado título nos gags

--- trunk/src/net/gags/MainActivity.java
+++ trunk/src/net/gags/MainActivity.java
@@ -2,69 +2,62 @@ package net.gags;
 
 import net.gags.model.Gags;
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends Activity implements OnClickListener
-{
-    
+public class MainActivity extends Activity implements OnClickListener, Gags.ChangeListener {
+
     private Gags gags;
-    
+
+    private TextView titleText;
     private ImageView gagView;
     private Button previousButton;
     private Button nextButton;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) 
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        gagView = (ImageView) findViewById(R.id.gag);
-        previousButton = (Button) findViewById(R.id.previousButton);
-        nextButton = (Button) findViewById(R.id.nextButton);
-        
-        previousButton.setOnClickListener(this);
-        nextButton.setOnClickListener(this);
-        
-        try
-        {
-            gags = new Gags(Gags.HOT_PAGE);
-            gags.refresh();
-            gagView.setImageBitmap(gags.getCurrentGag());
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
-        
+    public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.main);
+
+	titleText = (TextView) findViewById(R.id.titleText);
+	gagView = (ImageView) findViewById(R.id.gagView);
+	previousButton = (Button) findViewById(R.id.previousButton);
+	nextButton = (Button) findViewById(R.id.nextButton);
+
+	previousButton.setOnClickListener(this);
+	nextButton.setOnClickListener(this);
+
+        gags = new Gags(Gags.HOT_PAGE);
+        gags.addGagHandler(this);
+        gags.refresh();
     }
 
     @Override
-    public void onClick(View view)
-    {
-        try
-        {
-            switch(view.getId())
-            {
-                case R.id.previousButton:
-                    gags.previous();
-                    gagView.setImageBitmap(gags.getCurrentGag());
-                    break;
-                    
-                case R.id.nextButton:
-                    gags.next();
-                    gagView.setImageBitmap(gags.getCurrentGag());
-                    break;
-            }
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
+    public void onClick(View view) {
+	try {
+	    switch (view.getId()) {
+	    case R.id.previousButton:
+		gags.previous();
+		break;
+
+	    case R.id.nextButton:
+		gags.next();
+		break;
+	    }
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
     }
-    
+
+    @Override
+    public void setGag(String title, Bitmap image) {
+        titleText.setText(title);
+        gagView.setImageBitmap(image);
+    }
+
 }
--- trunk/src/net/gags/util/HtmlUtil.java
+++ trunk/src/net/gags/util/HtmlUtil.java
@@ -8,21 +8,20 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-public class HtmlUtil
-{
+public class HtmlUtil {
 
-    public static Elements get(String url, String query) throws IOException
-    {
-        Document doc = Jsoup.connect(url).get();
-        return doc.select(query);
+    public static Elements get(String url, String query) throws IOException {
+	Document doc = Jsoup.connect(url).get();
+	return doc.select(query);
     }
-    
-    public static String getAttributeValue(Element e, String tag, String attr)
-    {
-        Elements elem = e.getElementsByTag(tag);
-        Iterator<Element> i = elem.iterator();
-        if(i.hasNext()) return i.next().attr(attr);
-        else return null;
+
+    public static String getAttributeValue(Element e, String tag, String attr) {
+	Elements elem = e.getElementsByTag(tag);
+	Iterator<Element> i = elem.iterator();
+	if (i.hasNext())
+	    return i.next().attr(attr);
+	else
+	    return null;
     }
-    
+
 }
