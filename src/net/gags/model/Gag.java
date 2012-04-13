aceaeb15d1783f92f11a05c6e1dc857626d55f07
vitor.muniz.carvalho <vitor.muniz.carvalho@gmail.com>
2012-04-13 01:05:04 +0000

    Adicionado título nos gags

--- src/net/gags/model/Gag.java
+++ src/net/gags/model/Gag.java
@@ -3,7 +3,6 @@ package net.gags.model;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import android.graphics.Bitmap;
@@ -12,65 +11,56 @@ import android.graphics.BitmapFactory;
 import android.os.Environment;
 import android.util.Log;
 
-public class Gag
-{
-    
-    private static String localDir; 
-    
+public class Gag {
+
+    private static String localDir;
+
     private int id;
     private String url;
     private String title;
     private String localUrl;
-    
-    static
-    {
-        localDir = Environment.getExternalStorageDirectory().getAbsolutePath()+"/9Gags/gags/";
+
+    static {
+        localDir = Environment.getExternalStorageDirectory().getAbsolutePath() + "/9Gags/gags/";
         new File(localDir).mkdirs();
     }
-    
-    public Gag(int id, String url, String title) throws MalformedURLException, IOException
-    {
+
+    public Gag(int id, String url, String title) throws IOException {
         this.id = id;
         this.url = url;
         this.title = title;
-        this.localUrl = localDir+id+".jpg";
-        
+        this.localUrl = localDir + id + ".jpg";
+
         Log.i("LocalURL", localUrl);
     }
-    
-    private Bitmap requestGag() throws MalformedURLException, IOException
-    {
+
+    private Bitmap requestGag() throws IOException {
         Bitmap bitmap = BitmapFactory.decodeStream(new URL(url).openStream());
         bitmap.compress(CompressFormat.JPEG, 100, new FileOutputStream(localUrl));
         return bitmap;
     }
-    
-    public Bitmap getBitmap() throws MalformedURLException, IOException
-    {
-        if(new File(localUrl).exists())
+
+    public Bitmap getBitmap() throws IOException {
+        if (new File(localUrl).exists())
             return BitmapFactory.decodeFile(localUrl);
-        else return requestGag();
-    }
-    
-    public int getId()
-    {
-        return id;
-    }
-    
-    public String getUrl()
-    {
-        return url;
-    }
-    
-    public String getLocalUrl()
-    {
-        return localUrl;
-    }
-    
-    public String getTitle()
-    {
-        return title;
+        else
+            return requestGag();
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getLocalUrl() {
+        return localUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 
 }
--- src/net/gags/model/Gags.java
+++ src/net/gags/model/Gags.java
@@ -1,7 +1,6 @@
 package net.gags.model;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -11,103 +10,105 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import android.graphics.Bitmap;
-import android.os.Handler;
-import android.util.Log;
 
-public class Gags
-{
-    
+public class Gags {
+
     public static final String HOT_PAGE = "http://9gag.com";
     public static final String TRENDING_PAGE = "http://9gag.com/trending";
     public static final String VOTE_PAGE = "http://9gag.com/vote";
-    
+
     private String page;
     private ArrayList<Gag> gags;
     private int index;
-    
-    private ArrayList<Handler> gagHandlers;
-    
-    
-    public Gags(String page) throws IOException
-    {
+
+    private ArrayList<ChangeListener> changeListeners;
+
+    public static interface ChangeListener {
+        void setGag(String title, Bitmap image);
+    }
+
+    public Gags(String page) {
         this.page = page;
         gags = new ArrayList<Gag>();
-        gagHandlers = new ArrayList<Handler>();
+        changeListeners = new ArrayList<ChangeListener>();
         index = 0;
     }
-    
-    public void refresh() throws IOException
-    {
-        Elements elem = HtmlUtil.get(page, "div.content");
-        Iterator<Element> i = elem.iterator();
-        
-        while(i.hasNext())
-        {
-            Element e = i.next();
-            
-            try
-            {
-                int id = getGagID(e);
-                String url = getGagURL(e);
-                String title = getGagTitle(e);
-                
-                Gag gag = new Gag(id, url, title); 
-                gags.add(gag);
-                Log.i("Added", gag.getLocalUrl());
+
+    public void refresh() {
+        try {
+            Elements elem = HtmlUtil.get(page, "div.content");
+            Iterator<Element> i = elem.iterator();
+
+            while (i.hasNext()) {
+                Element e = i.next();
+
+                try {
+                    int id = getGagID(e);
+                    String url = getGagURL(e);
+                    String title = getGagTitle(e);
+
+                    Gag gag = new Gag(id, url, title);
+                    gags.add(gag);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
             }
-            catch(Exception ex) 
-            {
-                Log.e("Error", ex.getMessage());
+
+            notifyChangeListeners();
+        } catch (Exception ex) {
+            
+        }
+    }
+
+    public void previous() {
+        if (index > 0) {
+            index--;
+            try {
+                notifyChangeListeners();
+            } catch (Exception ex) {
+                index++;
             }
         }
+    }
+
+    public void next() {
+        if (index < gags.size() - 1) {
+            index++;
+            try {
+                notifyChangeListeners();
+            } catch (Exception ex) {
+                index--;
+            }
+        }
+    }
+
+    private void notifyChangeListeners() throws IOException {
+        Gag gag = gags.get(index);
+        String title = gag.getTitle();
+        Bitmap bitmap = gag.getBitmap();
         
-        notifyGagHandlers();
+        for (ChangeListener listener : changeListeners)
+            listener.setGag(title, bitmap);
     }
-    
-    public Bitmap getCurrentGag() throws MalformedURLException, IOException
-    {
-        return gags.get(index).getBitmap();
+
+    public void addGagHandler(ChangeListener listener) {
+        changeListeners.add(listener);
     }
-    
-    public void previous()
-    {
-        if(index > 0) index--;
+
+    public void removeGagHandler(ChangeListener listener) {
+        changeListeners.remove(listener);
     }
-    
-    public void next()
-    {
-        if(index < gags.size()-1) index++;
-    }
-    
-    private void notifyGagHandlers()
-    {
-//        for(Handler handler : gagHandlers)
-//            handler.
-    }
-    
-    public void addGagHandler(Handler handler)
-    {
-        gagHandlers.add(handler);
-    }
-    
-    public void removeGagHandler(Handler handler)
-    {
-        gagHandlers.remove(handler);
-    }
-    
-    private int getGagID(Element e)
-    {
+
+    private int getGagID(Element e) {
         String id = HtmlUtil.getAttributeValue(e, "a", "href").substring(5);
         return Integer.parseInt(id);
     }
-    
-    private String getGagURL(Element e)
-    {
+
+    private String getGagURL(Element e) {
         return HtmlUtil.getAttributeValue(e, "img", "src");
     }
-    
-    private String getGagTitle(Element e)
-    {
+
+    private String getGagTitle(Element e) {
         return HtmlUtil.getAttributeValue(e, "img", "alt");
     }
 
